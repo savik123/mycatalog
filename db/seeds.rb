6583ce76af203d57f610638d3372d4ed9765cca7
S F <1savafilatov1@gmail.com>
2026-03-29 19:32:37 +0500

    add images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,11 @@
-puts "🚀 SEED START"
-
-Equipment.destroy_all
-
 Equipment.create!(
   name: "CreatBot D600 Pro",
   purpose: "Крупноформатная 3D-печать",
   description: "Промышленный 3D-принтер",
   keywords: "3d принтер печать"
-)
-
-Equipment.create!(
-  name: "Epson",
-  purpose: "Печать документов",
-  description: "Офисный принтер",
-  keywords: "принтер печать"
-)
-
-puts "✅ CREATED: #{Equipment.count}"
+).tap do |e|
+  e.images.attach(
+    io: File.open(Rails.root.join("app/assets/images/equipment/creatbot.jpg")),
+    filename: "creatbot.jpg"
+  )
+end
