b65445cdb6c26588e55c423ad5e95dda800a671c
S F <1savafilatov1@gmail.com>
2026-03-29 20:05:36 +0500

    improve image matching

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 puts "🚀 CREATE EQUIPMENT"
 
 Equipment.destroy_all
-
+mapping = {
+  "proma" => "Proma FP-20A",
+  "roven" => "Вентилятор осевой РОВЕН",
+  "jetdisc" => "JET Disc & Belt Sander JSG-64"
+}
 equipments = [
   { name: "JET Metal Lathe BD-10VS", purpose: "Токарная обработка металла", description: "Настольный токарный станок", keywords: "станок токарный металл" },
   { name: "Anycubic Photon M3 Max", purpose: "3D-печать (смола)", description: "3D-принтер для печати из фотополимерной смолы", keywords: "3d принтер смола" },
@@ -43,22 +47,29 @@ puts "✅ CREATED: #{Equipment.count}"
 puts "🖼 ATTACH IMAGES"
 
 folder = Rails.root.join('app/assets/images/equipment')
+mapped = mapping.find { |key, _| filename.downcase.include?(key) }
 
-def normalize(str)
-  str.downcase.gsub(/[^a-z0-9а-яё]/i, '')
+if mapped
+  equipment = Equipment.find_by(name: mapped[1])
+else
+  # обычный поиск
+end
+def normalize_words(str)
+  str.downcase.gsub(/[^a-z0-9а-яё\s]/i, '').split
 end
 
 Dir.glob(folder.join('*')).each do |file_path|
   filename = File.basename(file_path, '.*')
-  normalized_filename = normalize(filename)
+  file_words = normalize_words(filename)
 
   equipment = Equipment.all.find do |e|
-    normalized_name = normalize(e.name)
-    normalized_name.include?(normalized_filename) || normalized_filename.include?(normalized_name)
+    name_words = normalize_words(e.name)
+
+    # ищем пересечение слов
+    (file_words & name_words).any?
   end
 
   if equipment
-    # чтобы не было дублей
     unless equipment.images.attached?
       equipment.images.attach(
         io: File.open(file_path),
